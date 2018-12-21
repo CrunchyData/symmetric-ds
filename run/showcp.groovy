cc86588de1057db95fbbde7c60dbcffbd3c497b4
Dave Cramer <davecramer@gmail.com>
2018-12-21 11:23:48 -0500

    show classpath for debugging

---
+++ run/showcp.groovy
@@ -0,0 +1,11 @@
+#!/usr/bin/env groovy
+def printClassPath(classLoader) {
+  println "$classLoader"
+  classLoader.getURLs().each {url->
+     println "- ${url.toString()}"
+  }
+  if (classLoader.parent) {
+     printClassPath(classLoader.parent)
+  }
+}
+printClassPath this.class.classLoader
